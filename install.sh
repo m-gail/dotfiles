141b7638689bb45117d92a8fd7352ccba18542f9
m-gail <34513390+m-gail@users.noreply.github.com>
2021-10-22 22:20:31 +0200

    Added zsh config

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo apt install -y curl stow
 
 # zsh
 sudo apt install -y zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+chsh $USER -s /bin/zsh
 
 # vim
 sudo apt install -y vim
