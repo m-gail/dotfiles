639b52c80135cbdb104f82f8c1ee8833babb0fbf
m-gail <34513390+m-gail@users.noreply.github.com>
2021-10-22 22:49:50 +0200

    Fixed zsh-plugins directory not being created in home-directory

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt install -y curl stow
 
 # zsh
 sudo apt install -y zsh
-mkdir zsh-plugins
+mkdir ~/zsh-plugins
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/zsh-plugins/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/zsh-plugins/zsh-autosuggestions
