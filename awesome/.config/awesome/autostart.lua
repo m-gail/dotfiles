c28058d0eda9bf3e236d4d7aecd50e96459b9ce9
m-gail <34513390+m-gail@users.noreply.github.com>
2022-06-16 17:25:45 +0200

    Update Escape to Capslock keybind

--- awesome/.config/awesome/autostart.lua
+++ awesome/.config/awesome/autostart.lua
@@ -1,7 +1,6 @@
 local awful = require("awful")
 
 awful.spawn.with_shell("picom --experimental-backends")
-awful.spawn.with_shell('xmodmap -e "clear lock"')
-awful.spawn.with_shell('xmodmap -e "keysym Caps_Lock = Escape"')
+awful.spawn.with_shell("setxkbmap -option caps:escape")
 awful.spawn.with_shell('light-locker')
 
