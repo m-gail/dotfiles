a309909a6b2013ff5ec45947510487ab852d3d3b
m-gail <34513390+m-gail@users.noreply.github.com>
2022-09-28 22:00:16 +0200

    Add local bin to path

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -1,3 +1,4 @@
 export EDITOR=nvim
 export EXA_COLORS="ur=1;35:uw=1;34:ux=1;36:ue=1;32:gr=1;35:gw=1;34:gx=1;32:tr=1;35:tw=1;34:tx=1;32:uu=1;36:un=1;31:gu=1;36:gn=1;31"
 export FPATH=$FPATH:/usr/share/zsh/vendor-completions
+export PATH=$HOME/.local/bin:$PATH
