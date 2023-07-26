2a3756fb39d7dca30ab66cc3903f6242f8a92b3f
m-gail <34513390+m-gail@users.noreply.github.com>
2023-07-26 11:47:40 +0200

    Make polybar launch script dynamically determine the primary monitor

--- bspwm/.config/polybar/launch.sh
+++ bspwm/.config/polybar/launch.sh
@@ -3,16 +3,16 @@
 POLYBAR_CMD=$(which polybar)
 POLYBAR_MSG_CMD=$(which polybar-msg)
 
-# Use git version if installed
-if test -f ~/SourcePackages/polybar/bin/polybar; then
-    POLYBAR_MSG_CMD=~/SourcePackages/polybar/bin/polybar-msg
-    POLYBAR_CMD=~/SourcePackages/polybar/bin/polybar
+# Use git version if installed for systray module support
+if test -f ~/SourcePackages/polybar/build/bin/polybar; then
+    POLYBAR_MSG_CMD=~/SourcePackages/polybar/build/bin/polybar-msg
+    POLYBAR_CMD=~/SourcePackages/polybar/build/bin/polybar
 fi
 
 $POLYBAR_MSG_CMD cmd quit
 
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [ $m = "DP-4" ]; then
+    if [ $m = $(xrandr --query | grep " connected primary" | cut -d" " -f1) ]; then
         MONITOR=$m $POLYBAR_CMD main & disown
     else
         MONITOR=$m $POLYBAR_CMD secondary & disown
