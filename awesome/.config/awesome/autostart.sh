6ca96ebe6521110fd3cafe526578038c7b34cf66
m-gail <34513390+m-gail@users.noreply.github.com>
2023-11-04 14:10:26 +0100

    Sequence bindings

--- awesome/.config/awesome/autostart.sh
+++ awesome/.config/awesome/autostart.sh
@@ -7,4 +7,6 @@ setxkbmap us -variant altgr-intl -option caps:escape
 [ -z  "$(pgrep blueman-applet)" ] && blueman-applet &
 [ -z  "$(pgrep nextcloud)" ] && nextcloud --background &
 killall sxhkd
+~/.config/sxhkd/sequences/generate.py ~/.config/sxhkd/sequences/bindings "super + space" > ~/.config/sxhkd/sequences/sxhkdrc
+~/.config/sxhkd/sequences/generate.py ~/.config/sxhkd/sequences/bindings.local "super + space" >> ~/.config/sxhkd/sequences/sxhkdrc
 sxhkd -c ~/.config/sxhkd/sequences/sxhkdrc
