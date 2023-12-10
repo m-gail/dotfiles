a65c6472ac8d18511923f1c782d86713206607ba
m-gail <34513390+m-gail@users.noreply.github.com>
2023-12-10 10:24:01 +0100

    Org agenda view

--- doom/.config/doom/mgail/org.el
+++ doom/.config/doom/mgail/org.el
@@ -3,12 +3,26 @@
 (setq org-startup-with-inline-images t)
 (setq org-export-directory "./build/out")
 (setq org-latex-pdf-process
-    '("latexmk -auxdir=build/aux -outdir=build/out -pdflatex='pdflatex -interaction nonstopmode' -pdf -bibtex -f %f"))
+      '("latexmk -auxdir=build/aux -outdir=build/out -pdflatex='pdflatex -interaction nonstopmode' -pdf -bibtex -f %f"))
 (setq-default org-display-custom-times t)
 (setq org-time-stamp-custom-formats '("<%a %d.%m.%Y>" . "<%a %d.%m.%Y %H:%M>"))
 (setq org-agenda-files (directory-files-recursively "~/Nextcloud/Notes" "\\.org$"))
-(setq org-agenda-span 14)
 (setq org-agenda-start-on-weekday nil)
+
+(setq org-agenda-custom-commands
+      '(("v" "Agenda"
+         ((agenda "" ((org-agenda-span 12)
+                      (org-deadline-warning-days 0)
+                      (org-agenda-start-day "0d")))
+          (agenda "" ((org-agenda-time-grid nil)
+                      (org-agenda-span 1)
+                      (org-agenda-start-day "0d")
+                      (org-deadline-warning-days 31)
+                      (org-agenda-show-all-dates nil)
+                      (org-agenda-span-name)
+                      (org-agenda-entry-types '(:deadline)))))
+         ((org-agenda-prefix-format '((agenda . "   %11s%11t  %-40c  %b")))
+          (org-agenda-breadcrumbs-separator "  ")))))
 
 ;; unbind default org bindings
 (map! :leader "o" nil)
