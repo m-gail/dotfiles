268c199ab4124387419cf1bc5ddae4dbec329879
m-gail <34513390+m-gail@users.noreply.github.com>
2024-01-02 16:45:38 +0100

    Add notes tag

--- awesome/.config/awesome/tags.lua
+++ awesome/.config/awesome/tags.lua
@@ -1,5 +1,5 @@
 local awful = require("awful")
 
 awful.screen.connect_for_each_screen(function(s)
-    awful.tag({ " ", " ", "󰜏 ", "󰭹 ", "󰖟 ", "󱞐 ", " ", "󰍹 " }, s, awful.layout.layouts[1])
+    awful.tag({ " ", " ", "󰜏 ", "󰭹 ", "󰖟 ", "󱞐 ", " ", "󰍹 ", " " }, s, awful.layout.layouts[1])
 end)
