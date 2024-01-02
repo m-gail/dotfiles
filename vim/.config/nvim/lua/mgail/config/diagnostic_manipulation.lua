a96a823e0b07aa904b846aa319722dca6ab71363
m-gail <34513390+m-gail@users.noreply.github.com>
2024-01-02 16:46:41 +0100

    Change severity of prettier diagnostics and add buffer mru keymap

--- vim/.config/nvim/lua/mgail/config/diagnostic_manipulation.lua
+++ vim/.config/nvim/lua/mgail/config/diagnostic_manipulation.lua
@@ -1,9 +1,17 @@
+local function rewrite_prettier_to_hint(diagnostic)
+    if diagnostic.code == "prettier/prettier" then
+        diagnostic.severity = vim.diagnostic.severity.HINT
+    end
+    return false
+end
+
 require("diagnostic_manipulation").setup {
     blacklist = {
         require("diagnostic_manipulation.builtin.tsserver").tsserver_codes({ 6133, 6196 }),
         require("diagnostic_manipulation.builtin.eslint").eslint_parsing_error,
     },
     whitelist = {
-        require("diagnostic_manipulation.builtin.severity").change_severities("eslint_d", -1)
+        require("diagnostic_manipulation.builtin.severity").change_severities("eslint_d", -1),
+        rewrite_prettier_to_hint
     }
 }
--- vim/.config/nvim/lua/mgail/keymap/telescope.lua
+++ vim/.config/nvim/lua/mgail/keymap/telescope.lua
@@ -5,3 +5,4 @@ nmap("<leader>fs", function() require("telescope.builtin").lsp_dynamic_workspace
 nmap("<leader>fg", ":Telescope live_grep<CR>")
 nmap("<leader>fb", ":Telescope file_browser<CR>")
 nmap("<leader>fB", function() require("telescope").extensions.file_browser.file_browser({ path = "%:p:h" }) end)
+nmap("<leader>b", function() require('telescope.builtin').buffers({ sort_mru = true }) end)
