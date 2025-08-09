6c2e66db47f90a82170081fb6566e6b7f47a5fe7
m-gail <34513390+m-gail@users.noreply.github.com>
2025-08-09 14:24:05 +0200

    Android

--- zsh/.config/zsh/environment.zsh
+++ zsh/.config/zsh/environment.zsh
@@ -2,3 +2,5 @@ export PATH=$HOME/.local/bin:$PATH
 export EDITOR=$(which nvim)
 export EXA_COLORS="ur=1;35:uw=1;34:ux=1;36:ue=1;32:gr=1;35:gw=1;34:gx=1;32:tr=1;35:tw=1;34:tx=1;32:uu=1;36:un=1;31:gu=1;36:gn=1;31"
 export FPATH=$FPATH:/usr/share/zsh/vendor-completions
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/platform-tools
